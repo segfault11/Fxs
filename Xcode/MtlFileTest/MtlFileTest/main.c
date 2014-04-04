34717f768b8caa9a35e89c07839d045af41fd821
Arno Luebke <arno.luebke@googlemail.com>
2014-04-04 15:43:16 +0900

    finished some tests for the mtl loader

--- Xcode/MtlFileTest/MtlFileTest/main.c
+++ Xcode/MtlFileTest/MtlFileTest/main.c
@@ -12,11 +12,34 @@
 int main(int argc, const char * argv[])
 {
     FxsObjMtlFilePtr file = FxsObjMtlFileCreateWithFile("LighthouseColored.mtl");
+    FxsObjMaterialPtr mat = NULL;
+    const char* attrElem = NULL;
     
     if (!file)
     {
         printf("could not load file\n");
+        return 0;
     }
+    
+    mat = FxsObjMtlFileGetMaterialWithName(file, "Material.003");
+    
+    if (!mat)
+    {
+        printf("could not get material\n");
+        FxsObjMtlFileDestroy(&file);
+        return 0;
+    }
+    
+    attrElem = FxsObjMaterialGetAttributeElementAsString(mat, "Ks", 2);
+    
+    if (!attrElem)
+    {
+        printf("could not get attr elem\n");
+        FxsObjMtlFileDestroy(&file);
+        return 0;
+    }
+
+    printf("Ns = %s\n", attrElem);
     
     FxsObjMtlFileDestroy(&file);
     
